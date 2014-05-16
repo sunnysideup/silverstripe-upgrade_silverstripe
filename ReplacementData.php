0307b16078b0fc0f2939f4d5e99e41f6bcfa945f
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2014-05-16 07:43:31 +0000

    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/upgrade_silverstripe/trunk@5669 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- ReplacementData.php
+++ ReplacementData.php
@@ -785,11 +785,11 @@ class ReplacementData {
 
 			array('::set_',
 						':\n  ',
-						'consider setting statics through yml files - change MyClass:set_my_var(1) to Config::inst()->update->("MyClass", "my_var", 1); '),
+						'consider setting statics through yml files, see http://doc.silverstripe.com/framework/en/topics/configuration, OR change  MyClass:set_my_var(1) to Config::inst()->update->("MyClass", "my_var", 1); '),
 
 			array('::get_',
 						'::get_',
-						'consider getting statics using Config system... e.g. Config::inst()->get("MyClass", "MyVar");')
+						'consider getting statics using Config system... e.g. Config::inst()->get("MyClass", "MyVar"); - see http://doc.silverstripe.com/framework/en/topics/configuration')
 
 		);
 
